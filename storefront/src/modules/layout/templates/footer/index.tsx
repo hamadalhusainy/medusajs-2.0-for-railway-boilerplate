cd7a7368d1a2b582bbd70091ce39f7a64106fb96
hamadalhusainy <hamadalhusainy@icloud.com>
2026-04-02 15:24:07 +0300

    Update index.tsx

--- storefront/src/modules/layout/templates/footer/index.tsx
+++ storefront/src/modules/layout/templates/footer/index.tsx
@@ -145,7 +145,7 @@ export default async function Footer() {
         </div>
         <div className="flex w-full mb-16 justify-between text-ui-fg-muted">
           <Text className="txt-compact-small">
-            © {new Date().getFullYear()} Medusa Store. All rights reserved.
+            © {new Date().getFullYear()} San Garden Kuwait Store. All rights reserved.
           </Text>
           <MedusaCTA />
         </div>
